bec7cc3794d9231197cfbad63c1d94a1475616a5
simonjellvert <>
2023-12-20 10:04:46 +0000

    Add sizes to product model and size selector box to product template

--- products/models.py
+++ products/models.py
@@ -30,6 +30,7 @@ class Product(models.Model):
     )
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(
         max_digits=6,
         decimal_places=2
