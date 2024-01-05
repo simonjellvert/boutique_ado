f37b2e24df47dd0efd2b580f3ae21d9dcc918760
simonjellvert <>
2024-01-05 14:47:31 +0000

    Finished add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -18,8 +18,9 @@ def profile(request):
             messages.success(request, 'Profile updated successfully')
         else:
             messages.error(request, 'Profile update failed')
+    else:
+        form = UserProfileForm(instance=profile)
 
-    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
